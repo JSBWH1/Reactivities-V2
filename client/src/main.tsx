1fe4fa3f1d36c254d76030bfa00eddfc9c49c918
Joseph Scott - Bentley <Joseph.Scott-Bentley@whitehallgroup.co.uk>
2025-10-08 09:46:32 +0100

    End of Section 3

---
+++ client/src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+/* 
+
+We can see JavaScript code above document.getElementById('')
+
+This basically calls in our application with the id of 'root' 
+
+createRoot() is react functionality. 
+
+.render() effectively renders our react application.
+
+In this case, our react application starts in 'Strict' mode and then renders our app component. 
+
+*/ 
