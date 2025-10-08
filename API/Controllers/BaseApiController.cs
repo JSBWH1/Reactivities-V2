ee9b8e91d675ae990532f9378aa7a056220f006e
Joseph Scott - Bentley <Joseph.Scott-Bentley@whitehallgroup.co.uk>
2025-10-08 15:00:27 +0100

    End of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +8,20 @@ namespace API.Controllers
     [ApiController]
     public class BaseApiController : ControllerBase
     {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator service is unavailable");
     }
 }
+
+
+
+
+/*
+
+??= is the null-coalescing assignment operator. It assigns the value on the right to the variable on the left only if the variable on the left is null. 
+If the variable on the left is not null, it retains its original value.
+
+*/
